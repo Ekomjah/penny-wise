210ebcd1486282a1ce8576a3653be45ca5b92f55
Ekomjah Edet <ekomjahedet@gmail.com>
2026-09-07 09:06:37 +0100

    fix: update MongoDB URI and change morgan logging format to combined

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ function createApp() {
   const app = express();
   app.use(cors());
   app.use(express.json());
-  app.use(morgan("dev"));
+  app.use(morgan("combined"));
 
   app.get("/", (req, res) => {
     res.send("Hello World");
