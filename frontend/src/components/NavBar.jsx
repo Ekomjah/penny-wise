c8731af21a4888c80033d23339144b184f5a6109
Ekomjah Edet <ekomjahedet@gmail.com>
2026-09-09 23:13:19 +0100

    Add color transitions and theme toggle styling
    Remove code and counter CSS rules.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
+import { CircleUser, House } from 'lucide-react';
 
 export default function NavBar() {
   return (
     <nav className="navbar grid grid-cols-3 p-4">
       <Link to="/" className="col-span-1 text-left">
-        Home
+        <House/>
       </Link>
       <Link to="/dashboard" className="text-xl font-bold col-span-1">
         Penny Wise
       </Link>
       <div className="auth-links flex gap-4 col-span-1 justify-end">
-        <Link to="/profile">Profile</Link>
+        <Link to="/profile"><CircleUser/></Link>
         <Link to="/signup">Sign Up</Link>
         <Link to="/login">Log In</Link>
       </div>
--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -6,8 +6,13 @@ export default function ThemeToggle() {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <button onClick={toggleTheme} title={theme === 'dark'  ? 'Switch to light theme' : 'Switch to dark theme'}>
-      {theme === 'dark' ? <Sun /> :<Moon />}
+    <button
+      onClick={toggleTheme}
+      title={
+        theme === 'dark' ? 'Switch to light theme' : 'Switch to dark theme'
+      }
+    >
+      {theme === 'dark' ? <Sun /> : <Moon />}
     </button>
   );
 }
